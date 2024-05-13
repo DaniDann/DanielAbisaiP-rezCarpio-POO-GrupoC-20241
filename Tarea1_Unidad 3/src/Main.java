b30fdaa28028108b75fdbd56761c40141e3cea0f
dani <159456415+DaniDann@users.noreply.github.com>
2024-05-13 00:45:39 -0600

    Add files via upload

---
+++ Tarea1_Unidad 3/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//        Crear un programa para una tienda en el cual un cliente pueda hacer la compra de diversos productos,
+//        la tienda cuenta con 4 tipos de productos, electrodomesticos, limpieza, belleza y alimentos.
+//        Todos los productos anteriores cuentan con nombre, precio, fecha de importación y número de serie,
+//        además de un método para poder agregar y eliminar elementos al stock, por lo que deberán de compartir
+//        todos estos atributos y método. De igual forma, cada uno de ellos cuenta con atributos y métodos específicos.
+
+
+
+
+
+
+    }
+}
---
+++ Tarea1_Unidad 3/src/Sistema.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Sistema {
+
+    private String electrodomesticos;
+    private String limpieza;
+    private String belleza;
+    private double price;
+    private LocalDate fecha;
+    
+
+
+
+}
